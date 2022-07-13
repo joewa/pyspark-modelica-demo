7fc26c96b158b748f3836df72b75ae89584aee89
Joerg Wangemann <joerg.wangemann@gmail.com>
2022-07-14 00:49:17 +0200

    EDrives example builds with MSL from package omsl. Next steps TODO is the timeseries usage

--- conda_recipe/build.sh
+++ conda_recipe/build.sh
@@ -1,6 +1,6 @@
-mkdir $PREFIX/lib/omlibrary
-cp -R $SRC_DIR/Complex.mo $PREFIX/"lib/omlibrary/Complex 4.0.0.mo"
-cp -R $SRC_DIR/Modelica $PREFIX/"lib/omlibrary/Modelica 3.2.3"
-cp -R $SRC_DIR/ModelicaServices $PREFIX/"lib/omlibrary/ModelicaServices 4.0.0"
+# mkdir $PREFIX/lib/omlibrary
+# cp -R $SRC_DIR/Complex.mo $PREFIX/"lib/omlibrary/Complex 4.0.0.mo"
+# cp -R $SRC_DIR/Modelica $PREFIX/"lib/omlibrary/Modelica 3.2.3"
+# cp -R $SRC_DIR/ModelicaServices $PREFIX/"lib/omlibrary/ModelicaServices 4.0.0"
 python setup.py build
 python setup.py install --single-version-externally-managed --record=record.txt
